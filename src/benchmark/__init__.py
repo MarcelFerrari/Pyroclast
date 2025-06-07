904ee6feb6f0969cd6e5cd845b3191da479a91dd
AliSot2000 <alisot200@gmail.com>
2025-06-07 17:10:09 +0200

    + Inits

---
+++ src/benchmark/__init__.py
@@ -0,0 +1,17 @@
+"""
+Pyroclast: Scalable Geophysics Models
+https://github.com/MarcelFerrari/Pyroclast
+
+File: benchmark/__init__.py
+Description: File contains imports for the benchmark functionality
+
+Author: Alexander Sotoudeh
+Copyright (c) 2024 Marcel Ferrari.
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at https://mozilla.org/MPL/2.0/.
+"""     
+
+from benchmark.benchmark_wrapper import BaseBenchmark, BenchmarkVY, BenchmarkValidatorVY
+import benchmark.runner as runner
