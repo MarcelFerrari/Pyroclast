373a3a75067a6e03e022d39c2642b25e4cd67743
AliSot2000 <alisot200@gmail.com>
2025-06-08 00:01:02 +0200

    + Added setting of numb threads

--- src/benchmark/runner.py
+++ src/benchmark/runner.py
@@ -24,6 +24,7 @@ import warnings
 from typing import Callable, Type, Optional
 
 from git import Repo
+import numba as nb
 
 from benchmark.utils import dtf
 from benchmark.benchmark_validators import (BenchmarkType, BenchmarkResults, BenchmarkRun,
@@ -268,6 +269,10 @@ def main():
     if ns.samples > 1 and ns.profiling and not ns.quiet:
         warnings.warn("Profiling in Combination with multiple samples increases runtime drastically")
 
+    if ns.cpu is not None:
+        print(f"Setting number of threads")
+        nb.set_num_threads(ns.cpu)
+
     # Check git status
     staged, unstaged = check_git_status()
     if staged or unstaged:
@@ -300,7 +305,7 @@ def main():
         env=None if ns.no_env else os.environ,
     )
 
-    print(benchmark_run.model_dump())
+    print(benchmark_run.model_dump_json(indent=2))
 
 if __name__ == "__main__":
     main()
