e1339368c2f70b636edabccbc246b486e0c61369
AliSot2000 <alisot200@gmail.com>
2025-06-08 00:38:42 +0200

    + Fetch banner again

--- src/Pyroclast/string_util.py
+++ src/Pyroclast/string_util.py
@@ -2,7 +2,7 @@
 Pyroclast: Scalable Geophysics Models
 https://github.com/MarcelFerrari/Pyroclast
 
-File: format.py
+File: string_util.py
 Description: Utility functions for pretty formatting of numbers and strings.
 
 Author: Marcel Ferrari
@@ -12,20 +12,22 @@ This Source Code Form is subject to the terms of the Mozilla Public
 License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at https://mozilla.org/MPL/2.0/.
 """
+import termcolor
 
-def s2yr(seconds):
+
+def s2yr(seconds: float):
     """
     Format a time duration in seconds into yr, Myr, or Gyr.
-    
+
     Parameters:
         seconds (float): The time duration in seconds.
-    
+
     Returns:
         str: A formatted string with the time in the most appropriate unit.
     """
     # Constants
     SECONDS_IN_YEAR = 365.25 * 24 * 60 * 60  # Seconds in one year
-    
+
     # Convert to years
     years = seconds / SECONDS_IN_YEAR
 
@@ -42,3 +44,16 @@ def s2yr(seconds):
 
     # Format the result with two decimal places
     return f"{value:.2f} {unit}"
+
+
+banner = termcolor.colored(r"""
+ ______   ______   ___   ____ _        _    ____ _____ 
+|  _ \ \ / /  _ \ / _ \ / ___| |      / \  / ___|_   _|
+| |_) \ V /| |_) | | | | |   | |     / _ \ \___ \ | |  
+|  __/ | | |  _ <| |_| | |___| |___ / ___ \ ___) || |  
+|_|    |_| |_| \_\\___/ \____|_____/_/   \_\____/ |_|  (v0.1)
+""", 'red', attrs=['bold'])
+
+
+def print_banner():
+    print(banner)
