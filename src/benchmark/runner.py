f81f31bff9b1984b7a8afd627df140c981411c63
AliSot2000 <alisot200@gmail.com>
2025-06-07 23:01:12 +0200

    + Added function to get git info

--- src/benchmark/runner.py
+++ src/benchmark/runner.py
@@ -195,6 +195,21 @@ def check_git_status() -> tuple[bool, bool]:
     return has_staged_changes, has_unstaged_changes
 
 
+def get_git_info() -> tuple[str, str, str]:
+    """
+    Get branch and commit hash
+
+    :returns: <branch name>, <commit hash>, <commit message>
+    """
+    repo = Repo(".", search_parent_directories=True)
+
+    branch_name = repo.active_branch.name
+    commit_hash = repo.active_branch.commit.hexsha
+    commit_msg = repo.active_branch.commit.message
+
+    return branch_name, commit_hash, commit_msg
+
+
 def main():
     """
     Main function to make it runnable from other locations.
