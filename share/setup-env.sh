ade2771cbfd8071188d70840129eddaf7a6d7b6c
AliSot2000 <alisot200@gmail.com>
2025-06-07 16:41:45 +0200

    + More .gitignore

--- share/setup-env.sh
+++ share/setup-env.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+#
+# Pyroclast: Scalable Geophysics Models
+# https://github.com/MarcelFerrari/Pyroclast
+
+# File: share/setup-env.sh
+# Description: File contains setting of environemnt variables to get
+#              the shell env ready to run PyroFile contains setting of environemnt variables to get
+#                           the shell env ready to run Pyroclast
+# Author: Marcel Ferrari, Alexander Sotoudeh
+# Copyright (c) 2024 Marcel Ferrari.
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 # Get the current directory where the script is located
 SCRIPT_PATH=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 BIN_PATH=$(realpath "$SCRIPT_PATH/../bin/")
