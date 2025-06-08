800e0604a56ac8ec3469de5b28d6c0df7ed28801
AliSot2000 <alisot200@gmail.com>
2025-06-08 22:40:43 +0200

    + Added config for benchmarks

---
+++ src/benchmark/config.py
@@ -0,0 +1,23 @@
+import os
+from pydantic import BaseModel, Field
+
+
+class BenchmarkConfig(BaseModel):
+    results_store: str = Field("[absolute] path to where the benchmark results are stored.")
+    day_folders: bool = Field(True, description="Whether the benchmark results grouped by day and stored in "
+                                                "subfolders for each day.")
+    hash_suffix: bool = Field(True, description="Whether to add the json hash to the file name")
+
+
+if os.environ.get("PYROCLAST_BENCHMARK_CONFIG") is not None:
+    _config_path = os.path.abspath(os.environ.get("PYROCLAST_BENCHMARK_CONFIG"))
+else:
+    _config_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "scripts",
+                                                "benchmark_config.json"))
+
+if not os.path.exists(_config_path):
+    raise FileNotFoundError(f"Config file {_config_path} not found.")
+
+
+with open(_config_path, "r") as f:
+    config = BenchmarkConfig.model_validate_json(f.read())
