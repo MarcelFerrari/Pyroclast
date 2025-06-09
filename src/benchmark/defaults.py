35bbf468dfa58ca8038efb49d01d4ba12b676095
AliSot2000 <alisot200@gmail.com>
2025-06-09 17:55:17 +0200

    + Bugfix, relative import needed

--- src/benchmark/defaults.py
+++ src/benchmark/defaults.py
@@ -1,7 +1,7 @@
 """
 File contains default values for the arg parser of the benchmark runner.
 """
-from benchmark_validators import BenchmarkType
+from .benchmark_validators import BenchmarkType
 
 max_iter = 128
 number_of_samples = 1
