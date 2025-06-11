1b8c22572da7e9df79ef29f46e264cb8b91bc1c2
AliSot2000 <alisot200@gmail.com>
2025-06-11 16:16:50 +0200

    + Wrong argument

--- src/benchmark/results_processing.py
+++ src/benchmark/results_processing.py
@@ -196,7 +196,7 @@ def load_benchmark_run(path: str, config: Optional[BenchmarkConfig] = None) -> B
     hash_string = os.path.splitext("_".join(hash_blocks))[0]
 
     # Compute hash and verify
-    if hash_string != benchmark_run_string_hasher(config):
+    if hash_string != benchmark_run_string_hasher(content):
         raise ValueError("File Contents were modified. File hash doesn't match.\n"
                          f"Expected: {hash_string}\n"
                          f"Actual: {benchmark_run_string_hasher(config)}")
