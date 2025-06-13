458e8f562ef4163194cc660e77b3894e21f48b95
AliSot2000 <alisot200@gmail.com>
2025-06-13 15:09:10 +0200

    + Added sanity check for inline gs

---
+++ src/Pyroclast/model/stokes_2D_mg/smoothers/vx/inline_vx_gs.py
@@ -0,0 +1,87 @@
+import os.path
+from typing import Type
+
+import numba as nb
+
+from Pyroclast.model.stokes_2D_mg.utils import apply_vx_BC
+from .inline_vx import compute_coeffs, compute_neighbor_sum
+
+
+# Gauss-Seidel update for vx
+@nb.njit(cache=True)
+def vx_gs_sweep(nx1, ny1,
+                dx, dy,
+                etap, etab,
+                vx, vy,
+                relax_v, rhs, BC):
+    """
+    In-place Red-Black Gauss-Seidel update for vx.
+    """
+    for i in range(1, ny1 - 1):
+        for j in range(1, nx1 - 2):
+            vx_c1, vx_c2, vx_c3, vx_c4, vx_c5, vy_c1, vy_c2, vy_c3, vy_c4 = compute_coeffs(i, j, dx, dy, etap, etab)
+
+            # Gauss-Seidel in-place update
+            vx[i, j] = compute_neighbor_sum(
+                i, j, relax_v, vx_c1, vx_c2, vx_c3, vx_c4, vx_c5, vy_c1, vy_c2, vy_c3, vy_c4, vx, vy, rhs
+            )
+
+    apply_vx_BC(vx, BC)
+
+    return vx
+
+
+# INFO need to use string references to avoid circular imports and deal with benchmark packaged not available
+def benchmark_factory() -> tuple[Type["BenchmarkSmoother"], Type["BenchmarkVX"], Type["BenchmarkVY"]]:
+    """
+    Returns Benchmark Classes needed for benchmarking. Done via factory to avoid issues with the `benchmark` package
+    not being available in a production environment.
+    """
+    import benchmark.benchmark_wrapper as bw
+    from benchmark.benchmark_validators import Stage, Timing
+    from benchmark.utils import dtf
+
+    module_name = os.path.basename(__file__).replace(".py", "")
+
+    class BaseImplementationVX(bw.BenchmarkVX):
+        def benchmark_preamble(self):
+            start = dtf()
+            vx_gs_sweep(nx1=self.nx1, ny1=self.ny1,
+                        dx=self.dx, dy=self.dy,
+                        etap=self.eta_p, etab=self.eta_b,
+                        vx=self.vx, vy=self.vy,
+                        relax_v=self.relax_v, BC=self.boundary_condition, rhs=self.vx_rhs)
+            end = dtf()
+
+            # Add the timing information
+            self.timings.append(Timing(name=f"{module_name}.{self.__class__.__name__}: Preamble",
+                                       stage=Stage.PREAMBLE,
+                                       start=start,
+                                       end=end))
+
+        def benchmark_epilogue(self):
+            """
+            No post-processing
+            """
+            pass
+
+        def run_benchmark(self):
+            """
+            Perform the actual run of the benchmark.
+            """
+            start = dtf()
+            for _ in range(self.args.max_iter):
+                vx_gs_sweep(nx1=self.nx1, ny1=self.ny1,
+                            dx=self.dx, dy=self.dy,
+                            etap=self.eta_p, etab=self.eta_b,
+                            vx=self.vx, vy=self.vy,
+                            relax_v=self.relax_v, BC=self.boundary_condition, rhs=self.vx_rhs)
+            end = dtf()
+
+            # Add the timing information
+            self.timings.append(Timing(name=f"{module_name}.{self.__class__.__name__}: Benchmark",
+                                       stage=Stage.BENCHMARK,
+                                       start=start,
+                                       end=end))
+
+    return None, BaseImplementationVX, None
