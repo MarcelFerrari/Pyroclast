6779f5c924137655752f6a8fe4f85b8fb80af3e9
AliSot2000 <alisot200@gmail.com>
2025-06-14 21:56:00 +0200

    + Implemented the printer script to dump bhe results to the console

--- src/benchmark/printer.py
+++ src/benchmark/printer.py
@@ -1,8 +1,8 @@
 import argparse
-import benchmark.results_processing as string_util
-from Pyroclast.string_util import print_banner
-from benchmark.benchmark_validators import BenchmarkRun, BenchmarkResults, BenchmarkType
 
+import benchmark.results_processing as res_proc
+from Pyroclast.string_util import print_banner
+from benchmark.config import get_config
 
 """
 File contains a script to fetch a given benchmark result. It then reproduces the output of the runner script if provided
@@ -11,3 +11,26 @@ with the -v option.
 
 
 parser = argparse.ArgumentParser()
+parser.add_argument("-b", "--benchmark-result",
+                    type=str,
+                    required=True,
+                    help="Benchmark result file (relative to results folder in config)")
+parser.add_argument("-c", "--config",
+                    type=str,
+                    default=None,
+                    help="Path to config file overrides default path and environment option")
+
+
+def main():
+    ns = parser.parse_args()
+    config = get_config(ns.config)
+
+    path = ns.benchmark_result
+
+    bmr = res_proc.load_benchmark_run(path, config)
+    res_proc.print_statistics(bmr)
+
+
+if __name__ == "__main__":
+    print_banner()
+    main()
