b2a8bd0556ff76b7ee0aeeb58e231fc5b320188a
AliSot2000 <alisot200@gmail.com>
2025-06-15 22:46:22 +0200

    + Implemented two different versions of staggered red-black gauss-seidel

--- src/Pyroclast/model/stokes_2D_mg/smoothers/vx/__init__.py
+++ src/Pyroclast/model/stokes_2D_mg/smoothers/vx/__init__.py
@@ -1,3 +1,14 @@
 """
-INFO: This package contains sub implementations that only compute the vx update. (needed to different vx update schemes)
+Pyroclast: Scalable Geophysics Models
+https://github.com/MarcelFerrari/Pyroclast
+
+File: Pyroclast/model/stokes_2D_mg/smoothers/vx/__init__.py
+Description: This package contains sub implementations that only compute the vx update. (needed to different vx update schemes)
+
+Author: Alexander Sotoudeh
+Copyright (c) 2024 Marcel Ferrari.
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at https://mozilla.org/MPL/2.0/.
 """
