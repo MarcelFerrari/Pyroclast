d5a58da5aa9d36e6e73b625fc90b14b6ac849dcf
AliSot2000 <alisot200@gmail.com>
2025-06-15 17:09:35 +0200

    + Renamed defaults + Added default for plots

--- src/benchmark/defaults.py
+++ src/benchmark/defaults.py
@@ -1,8 +1,14 @@
 """
 File contains default values for the arg parser of the benchmark runner.
 """
-from .benchmark_validators import BenchmarkType
+from .benchmark_validators import BenchmarkType, PlotType
 
 max_iter = 128
 number_of_samples = 1
-types = [BenchmarkType.SMOOTHER, BenchmarkType.VX, BenchmarkType.VY]
+benchmark_types = [BenchmarkType.SMOOTHER, BenchmarkType.VX, BenchmarkType.VY]
+plot_types = [PlotType.MODULE_VS_CPU_X_DIM,
+              PlotType.MODULE_VS_DIM_CPU16,
+              PlotType.MODULE_VS_DIM_CPU8,
+              PlotType.CPU_VS_MODULE_X_DIM,
+              PlotType.MODULE_X_CPU_VS_DIM,
+              PlotType.DIM_VS_MODULE_X_CPU]
