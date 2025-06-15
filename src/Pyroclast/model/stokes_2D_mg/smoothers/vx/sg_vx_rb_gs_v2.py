c1ba79b0c2106c506ef19b9f0e705460cbc52fe4
AliSot2000 <alisot200@gmail.com>
2025-06-15 22:46:22 +0200

    + Implemented two different versions of staggered red-black gauss-seidel

---
+++ src/Pyroclast/model/stokes_2D_mg/smoothers/vx/sg_vx_rb_gs_v2.py
@@ -0,0 +1,147 @@
+import os.path
+from typing import Type
+
+import numba as nb
+import numpy as np
+
+from Pyroclast.model.stokes_2D_mg.utils import apply_vx_BC, apply_vy_BC
+from .inline_vx import compute_coeffs, compute_neighbor_sum
+
+
+"""
+In this file we aim to test if the staggered approach with explicit setup of the pipeline and explicit tear down of the 
+pipeline is faster than compiling an if into the the for loop.
+"""
+
+
+@nb.njit(cache=True, parallel=True)
+def _vx_rb_gs_sweep(nx1, ny1,
+                    dx, dy,
+                    etap, etab,
+                    vx, vy,
+                    relax_v, rhs, BC) -> np.ndarray:
+    """
+    In-place Red-Black Gauss-Seidel update for vx.
+    """
+    # Setup pipeline with the first two rows of the red pass or red-black gauss-seidel
+    i = 1
+    j_start = 1 if i % 2 == 0 else 2  # Red pass starts on even (i+j)
+    for j in range(j_start, nx1 - 2, 2):
+        vx_c1, vx_c2, vx_c3, vx_c4, vx_c5, vy_c1, vy_c2, vy_c3, vy_c4 = compute_coeffs(i, j, dx, dy, etap, etab)
+
+        # Gauss-Seidel in-place update
+        vx[i, j] = compute_neighbor_sum(
+            i, j, relax_v, vx_c1, vx_c2, vx_c3, vx_c4, vx_c5, vy_c1, vy_c2, vy_c3, vy_c4, vx, vy, rhs
+        )
+
+    i = 1 + 1
+    j_start = 1 if i % 2 == 0 else 2  # Red pass starts on even (i+j)
+    for j in range(j_start, nx1 - 2, 2):
+        vx_c1, vx_c2, vx_c3, vx_c4, vx_c5, vy_c1, vy_c2, vy_c3, vy_c4 = compute_coeffs(i, j, dx, dy, etap, etab)
+
+        # Gauss-Seidel in-place update
+        vx[i, j] = compute_neighbor_sum(
+            i, j, relax_v, vx_c1, vx_c2, vx_c3, vx_c4, vx_c5, vy_c1, vy_c2, vy_c3, vy_c4, vx, vy, rhs
+        )
+
+    for i in nb.prange(1 + 2, ny1 - 1):
+        assert 1 < i <= ny1 -1, "Error with bounds - red pass"
+        j_start = 1 if i % 2 == 0 else 2  # Red pass starts on even (i+j)
+        for j in range(j_start, nx1 - 2, 2):
+            vx_c1, vx_c2, vx_c3, vx_c4, vx_c5, vy_c1, vy_c2, vy_c3, vy_c4 = compute_coeffs(i, j, dx, dy, etap, etab)
+
+            # Gauss-Seidel in-place update
+            vx[i, j] = compute_neighbor_sum(
+                i, j, relax_v, vx_c1, vx_c2, vx_c3, vx_c4, vx_c5, vy_c1, vy_c2, vy_c3, vy_c4, vx, vy, rhs
+            )
+
+        assert 1 < i - 2 <= ny1 -1, "Error with bounds - black pass"
+        j_start = 2 if (i - 2) % 2 == 0 else 1  # Black pass starts on odd (i+j)
+        for j in range(j_start, nx1 - 2, 2):
+            vx_c1, vx_c2, vx_c3, vx_c4, vx_c5, vy_c1, vy_c2, vy_c3, vy_c4 = compute_coeffs(i - 2, j, dx, dy, etap, etab)
+
+            # Gauss-Seidel in-place update
+            vx[i - 2, j] = compute_neighbor_sum(
+                i - 2, j, relax_v, vx_c1, vx_c2, vx_c3, vx_c4, vx_c5, vy_c1, vy_c2, vy_c3, vy_c4, vx, vy, rhs
+            )
+
+    # Tear down pipeline with last two rows of the black pass
+    i = nx1 - 1 - 2
+    j_start = 2 if (i - 2) % 2 == 0 else 1  # Black pass starts on odd (i+j)
+    for j in range(j_start, nx1 - 2, 2):
+        vx_c1, vx_c2, vx_c3, vx_c4, vx_c5, vy_c1, vy_c2, vy_c3, vy_c4 = compute_coeffs(i - 2, j, dx, dy, etap, etab)
+
+        # Gauss-Seidel in-place update
+        vx[i - 2, j] = compute_neighbor_sum(
+            i - 2, j, relax_v, vx_c1, vx_c2, vx_c3, vx_c4, vx_c5, vy_c1, vy_c2, vy_c3, vy_c4, vx, vy, rhs
+        )
+
+    i = nx1 - 1 - 1
+    j_start = 2 if (i - 2) % 2 == 0 else 1  # Black pass starts on odd (i+j)
+    for j in range(j_start, nx1 - 2, 2):
+        vx_c1, vx_c2, vx_c3, vx_c4, vx_c5, vy_c1, vy_c2, vy_c3, vy_c4 = compute_coeffs(i - 2, j, dx, dy, etap, etab)
+
+        # Gauss-Seidel in-place update
+        vx[i - 2, j] = compute_neighbor_sum(
+            i - 2, j, relax_v, vx_c1, vx_c2, vx_c3, vx_c4, vx_c5, vy_c1, vy_c2, vy_c3, vy_c4, vx, vy, rhs
+        )
+
+    apply_vx_BC(vx, BC)
+
+    return vx
+
+
+# INFO need to use string references to avoid circular imports and deal with benchmark packaged not available
+def benchmark_factory() -> tuple[Type["BenchmarkSmoother"], Type["BenchmarkVX"], Type["BenchmarkVY"]]:
+    """
+    Returns Benchmark Classes needed for benchmarking. Done via factory to avoid issues with the `benchmark` package
+    not being available in a production environment.
+    """
+    import benchmark.benchmark_wrapper as bw
+    from benchmark.benchmark_validators import Stage, Timing
+    from benchmark.utils import dtf
+
+    module_name = os.path.basename(__file__).replace(".py", "")
+
+    class BaseImplementationVX(bw.BenchmarkVX):
+        def benchmark_preamble(self):
+            start = dtf()
+            _vx_rb_gs_sweep(nx1=self.nx1, ny1=self.ny1,
+                            dx=self.dx, dy=self.dy,
+                            etap=self.eta_p, etab=self.eta_b,
+                            vx=self.vx, vy=self.vy,
+                            relax_v=self.relax_v, BC=self.boundary_condition, rhs=self.vx_rhs)
+            end = dtf()
+
+            # Add the timing information
+            self.timings.append(Timing(name=f"{module_name}.{self.__class__.__name__}: Preamble",
+                                       stage=Stage.PREAMBLE,
+                                       start=start,
+                                       end=end))
+
+        def benchmark_epilogue(self):
+            """
+            No post-processing
+            """
+            pass
+
+        def run_benchmark(self):
+            """
+            Perform the actual run of the benchmark.
+            """
+            start = dtf()
+            for _ in range(self.args.max_iter):
+                _vx_rb_gs_sweep(nx1=self.nx1, ny1=self.ny1,
+                                dx=self.dx, dy=self.dy,
+                                etap=self.eta_p, etab=self.eta_b,
+                                vx=self.vx, vy=self.vy,
+                                relax_v=self.relax_v, BC=self.boundary_condition, rhs=self.vx_rhs)
+            end = dtf()
+
+            # Add the timing information
+            self.timings.append(Timing(name=f"{module_name}.{self.__class__.__name__}: Benchmark",
+                                       stage=Stage.BENCHMARK,
+                                       start=start,
+                                       end=end))
+
+    return None, BaseImplementationVX, None
