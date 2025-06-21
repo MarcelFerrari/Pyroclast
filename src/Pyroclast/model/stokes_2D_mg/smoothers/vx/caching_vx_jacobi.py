e9e4e8c7a6e5b3fa571bf635f6b3dc032ab329ea
AliSot2000 <alisot200@gmail.com>
2025-06-21 14:55:45 +0200

    + Caching didn't work with dynamic allocation of data structure

--- src/Pyroclast/model/stokes_2D_mg/smoothers/vx/caching_vx_jacobi.py
+++ src/Pyroclast/model/stokes_2D_mg/smoothers/vx/caching_vx_jacobi.py
@@ -5,7 +5,7 @@ import numba as nb
 import numpy as np
 
 from Pyroclast.model.stokes_2D_mg.utils import apply_vx_BC
-from .inline_vx import compute_coeffs, compute_neighbor_sum, prep_vx_cache
+from .inline_vx import compute_neighbor_sum, prep_vx_cache
 
 
 """
@@ -18,24 +18,15 @@ In this file we investigate the speed of the jacobi solver given that it operate
 # Jacobi update for vx
 @nb.njit(cache=True, parallel=True)
 def vx_jacobi_sweep(nx1: int, ny1: int,
-                    dx: float, dy: float,
-                    etap: np.ndarray, etab: np.ndarray,
                     vx: np.ndarray, vy: np.ndarray,
                     vx_new: np.ndarray,
                     relax_v:float, BC: float, rhs: np.ndarray,
-                    vx_cache: np.ndarray = None) -> tuple[np.ndarray, np.ndarray]:
-    if vx_cache is None:
-        vx_cache = np.ndarray((ny1, nx1, 9), dtype=np.float64)
-
-        vx_cache = prep_vx_cache(nx1=nx1, ny1=ny1,
-                                 dx=dx, dy=dy,
-                                 etab=etab, etap=etap,
-                                 vx_cache=vx_cache)
+                    vx_cache: np.ndarray) -> np.ndarray:
 
     # Loop only over the interior cells
     for i in nb.prange(1, ny1 - 1):
         for j in range(1, nx1 - 2):
-            vx_c1, vx_c2, vx_c3, vx_c4, vx_c5, vy_c1, vy_c2, vy_c3, vy_c4 = compute_coeffs(i, j, dx, dy, etap, etab)
+            vx_c1, vx_c2, vx_c3, vx_c4, vx_c5, vy_c1, vy_c2, vy_c3, vy_c4 = vx_cache[i, j]
 
             # Jacobi update
             vx_new[i, j] = compute_neighbor_sum(
@@ -46,7 +37,7 @@ def vx_jacobi_sweep(nx1: int, ny1: int,
 
     apply_vx_BC(vx, BC)
 
-    return vx, vx_cache
+    return vx
 
 
 # INFO need to use string references to avoid circular imports and deal with benchmark packaged not available
@@ -67,11 +58,13 @@ def benchmark_factory() -> tuple[Type["BenchmarkSmoother"], Type["BenchmarkVX"],
             if self.vx_new is None:
                 self.vx_new = np.zeros((self.nx1, self.ny1))
 
+            self.vx_cache = np.zeros((self.nx1, self.ny1))
+
         def benchmark_preamble(self):
             start = dtf()
+            self.vx_cache = prep_vx_cache(self.vx_cache)
+
             vx_jacobi_sweep(nx1=self.nx1, ny1=self.ny1,
-                            dx=self.dx, dy=self.dy,
-                            etap=self.eta_p, etab=self.eta_b,
                             vx=self.vx, vy=self.vy, vx_new=self.vx_new,
                             relax_v=self.relax_v, BC=self.boundary_condition, rhs=self.vx_rhs)
             end = dtf()
@@ -93,14 +86,14 @@ def benchmark_factory() -> tuple[Type["BenchmarkSmoother"], Type["BenchmarkVX"],
             Perform the actual run of the benchmark.
             """
             start = dtf()
-            vx_cache = None
+            self.vx_cache = np.zeros((self.nx1, self.ny1))
+            self.vx_cache = prep_vx_cache(self.vx_cache)
+
             for _ in range(self.args.max_iter):
-                vx, vx_cache = vx_jacobi_sweep(nx1=self.nx1, ny1=self.ny1,
-                                               dx=self.dx, dy=self.dy,
-                                               etap=self.eta_p, etab=self.eta_b,
-                                               vx=self.vx, vy=self.vy, vx_new=self.vx_new,
-                                               relax_v=self.relax_v, BC=self.boundary_condition, rhs=self.vx_rhs,
-                                               vx_cache=vx_cache)
+                vx = vx_jacobi_sweep(nx1=self.nx1, ny1=self.ny1,
+                                     vx=self.vx, vy=self.vy, vx_new=self.vx_new,
+                                     relax_v=self.relax_v, BC=self.boundary_condition, rhs=self.vx_rhs,
+                                     vx_cache=self.vx_cache)
             end = dtf()
 
             # Add the timing information
