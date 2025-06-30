edaa1ccb346c77524f9f24d63f1e86ef8986d393
AliSot2000 <alisot200@gmail.com>
2025-06-30 11:06:45 +0200

    + Added a new package vor vy

---
+++ src/Pyroclast/model/stokes_2D_mg/smoothers/vy/__init__.py
@@ -0,0 +1,14 @@
+"""
+Pyroclast: Scalable Geophysics Models
+https://github.com/MarcelFerrari/Pyroclast
+
+File: Pyroclast/model/stokes_2D_mg/smoothers/vy/__init__.py 
+Description: Init file for vy smoother package
+
+Author: Alexander Sotoudeh
+Copyright (c) 2024 Marcel Ferrari.
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at https://mozilla.org/MPL/2.0/.
+"""
