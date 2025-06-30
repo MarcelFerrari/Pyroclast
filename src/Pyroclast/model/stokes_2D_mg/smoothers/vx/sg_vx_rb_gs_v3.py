9fa64fded51ee0008ed59e9aee18b66f269a9079
AliSot2000 <alisot200@gmail.com>
2025-06-30 20:43:35 +0200

    + Imports

--- src/Pyroclast/model/stokes_2D_mg/smoothers/vx/sg_vx_rb_gs_v3.py
+++ src/Pyroclast/model/stokes_2D_mg/smoothers/vx/sg_vx_rb_gs_v3.py
@@ -4,9 +4,8 @@ from typing import Type
 import numba as nb
 import numpy as np
 
-from Pyroclast.model.stokes_2D_mg.utils import apply_vx_BC, apply_vy_BC
+from Pyroclast.model.stokes_2D_mg.utils import apply_vx_BC
 from .inline_vx import compute_coeffs, compute_neighbor_sum
-
 
 """
 In this file we aim to test if the staggered approach with explicit setup of the pipeline and explicit tear down of the 
