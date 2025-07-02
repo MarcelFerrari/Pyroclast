564b78351165342e19a7fe0f908a1ad29eebcceb
AliSot2000 <alisot200@gmail.com>
2025-07-02 15:56:26 +0200

    + Added method that contains full loop body (for ease of use)

--- src/Pyroclast/model/stokes_2D_mg/smoothers/vx/inline_vx.py
+++ src/Pyroclast/model/stokes_2D_mg/smoothers/vx/inline_vx.py
@@ -56,6 +56,24 @@ def compute_neighbor_sum(i: int, j: int, relax_v: float,
     return (1.0 - relax_v) * vx[i, j] + relax_v * (rhs[i, j] - sum_neighbors) / diag
 
 
+@nb.njit(cache=True, inline="always")
+def inline_loop_body_vx(i: int, j: int,
+                        dx: float, dy: float,
+                        etap: np.ndarray, etab: np.ndarray,
+                        vx: np.ndarray, vy: np.ndarray, rhs: np.ndarray) -> float:
+    """
+    Contains full loop body for vx pass.
+    """
+    vx_c1, vx_c2, vx_c3, vx_c4, vx_c5, vy_c1, vy_c2, vy_c3, vy_c4 = compute_coeffs(i=i, j=j,
+                                                                                   dx=dx, dy=dy,
+                                                                                   etap=etap, etab=etab)
+
+    return compute_neighbor_sum(i=i, j=j,
+                                vx=vx, vy=vy, rhs=rhs,
+                                vy_c1=vy_c1, vy_c2=vy_c2, vy_c3=vy_c3, vy_c4=vy_c4,
+                                vx_c1=vx_c1, vx_c2=vx_c2, vx_c3=vx_c3, vx_c4=vx_c4, vx_c5=vx_c5)
+
+
 @nb.njit(cache=True, parallel=True, inline="always")
 def prep_vx_cache(nx1: int, ny1: int,
                   dx: float, dy: float,
