fb0c571a724e4821c36390d4f6518f956d070d98
AliSot2000 <alisot200@gmail.com>
2025-07-02 16:05:29 +0200

    + Bugfix, forgot staggering

--- src/Pyroclast/model/stokes_2D_mg/smoothers/vx/mk_sg_vx_rb_gs_v1.py
+++ src/Pyroclast/model/stokes_2D_mg/smoothers/vx/mk_sg_vx_rb_gs_v1.py
@@ -71,7 +71,7 @@ def _vx_rb_gs_sweep(nx1, ny1,
                 end_b = end_y if b + 1 == blocks else start_y + (b + 1) * cache_a
 
                 # Iterate through j
-                for j in range(1, nx1 - 2):
+                for j in range(1, nx1 - 2 + 2):
                     for i in range(start_b, end_b):
                         # Red pass
                         if 1 <= j <= nx1 - 2 and (i + j) % 2 == 0:
