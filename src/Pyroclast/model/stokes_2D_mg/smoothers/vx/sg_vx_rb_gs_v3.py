4cccd606c58b9ba6f2d3a1b36c88ec6154eef828
AliSot2000 <alisot200@gmail.com>
2025-08-25 17:29:57 +0200

    + Added information what exactly it is we're computing

--- src/Pyroclast/model/stokes_2D_mg/smoothers/vx/sg_vx_rb_gs_v3.py
+++ src/Pyroclast/model/stokes_2D_mg/smoothers/vx/sg_vx_rb_gs_v3.py
@@ -27,6 +27,7 @@ def _vx_rb_gs_sweep(nx1, ny1,
     # Setup pipeline with the first two rows of the red pass or red-black gauss-seidel
     # Less cells than threads
     if th < (nx1 - 4) / 2:
+        # INFO: This is a serial setup
         i = 1
         j_start = 1 if i % 2 == 0 else 2  # Red pass starts on even (i+j)
         for j in range(j_start, nx1 - 2, 2):
@@ -47,6 +48,7 @@ def _vx_rb_gs_sweep(nx1, ny1,
                 i, j, relax_v, vx_c1, vx_c2, vx_c3, vx_c4, vx_c5, vy_c1, vy_c2, vy_c3, vy_c4, vx, vy, rhs
             )
     else:
+        # INFO: This is a parallel setup
         for p in nb.prange(th):
             start = p * (nx1 - 3) / th
             end = (nx1 - 2) if p + 1 == th else (p + 1) * (nx1 - 3) / th
@@ -65,6 +67,7 @@ def _vx_rb_gs_sweep(nx1, ny1,
                         i, j, relax_v, vx_c1, vx_c2, vx_c3, vx_c4, vx_c5, vy_c1, vy_c2, vy_c3, vy_c4, vx, vy, rhs
                     )
 
+    # INFO: Staggered Loop Passes with Thread Blocking
     for i in nb.prange(1 + 2, ny1 - 1):
         # assert 1 <= i < ny1 -1, "Error with bounds - red pass"
         j_start = 1 if i % 2 == 0 else 2  # Red pass starts on even (i+j)
@@ -87,6 +90,7 @@ def _vx_rb_gs_sweep(nx1, ny1,
             )
 
     if th < (nx1 - 4) / 2:
+        # INFO: This is a serial setup
         # Tear down pipeline with last two rows of the black pass
         i = nx1 - 1 - 2
         j_start = 2 if (i - 2) % 2 == 0 else 1  # Black pass starts on odd (i+j)
@@ -109,6 +113,7 @@ def _vx_rb_gs_sweep(nx1, ny1,
             )
 
     else:
+        # INFO: This is a parallel setup
         for p in nb.prange(th):
             start = p * (nx1 - 3) / th
             end = (nx1 - 2) if p + 1 == th else (p + 1) * (nx1 - 3) / th
