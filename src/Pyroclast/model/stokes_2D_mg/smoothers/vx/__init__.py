ab62929096eedd9d8660a50e8c6c21fd9a20efb6
AliSot2000 <alisot200@gmail.com>
2025-08-25 17:02:01 +0200

    + Exported functions through __init__

--- src/Pyroclast/model/stokes_2D_mg/smoothers/vx/__init__.py
+++ src/Pyroclast/model/stokes_2D_mg/smoothers/vx/__init__.py
@@ -1,3 +1,4 @@
 """
 INFO: This package contains sub implementations that only compute the vx update. (needed to different vx update schemes)
 """
+from _inline_vx import inline_loop_body_vx, prep_vx_cache, compute_coeffs, compute_neighbor_sum
