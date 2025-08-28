b32ebaeaf3180c03110559386331001d8bf328dc
AliSot2000 <alisot200@gmail.com>
2025-08-28 16:53:35 +0200

    + Next iteration

---
+++ src/Pyroclast/model/stokes_2D_mg/smoothers/at_06.py
@@ -0,0 +1,74 @@
+import numba as nb
+import numpy as np
+import  os
+from typing import Type
+import math
+
+from Pyroclast.model.stokes_2D_mg.smoothers.vx import inline_loop_body_vx
+from Pyroclast.model.stokes_2D_mg.smoothers.vy import inline_loop_body_vy
+from Pyroclast.model.stokes_2D_mg.utils import apply_vx_BC, apply_vy_BC
+from Pyroclast.model.stokes_2D_mg.smoothers.base_rb_gs import velocity_smoother_rb_gs
+
+"""
+Attempt 6: Implemented Blocking Jacobi. And Thread Blocking. Cascading VY
+"""
+
+@nb.njit(cache=True, parallel=True)
+def velocity_smoother_jacobi(nx1: int, ny1: int,
+                             dx: float, dy: float,
+                             etap: np.ndarray, etab: np.ndarray,
+                             vx: np.ndarray, vy: np.ndarray,
+                             relax_v: float, BC: float,
+                             vx_rhs: np.ndarray, vy_rhs: np.ndarray, max_iter: int,
+                             vx_new: np.ndarray, vy_new: np.ndarray,
+                             th: int, cache_a: int, step_size: int) -> tuple[np.ndarray, np.ndarray]:
+    # Fast Implementation for big problems
+    if th * cache_a > nx1 - 2:
+        for _ in range(max_iter // 2 * 2):
+            # Work Split
+            for p in nb.prange(th):
+                start_x = p * (nx1 - 2) // th + 1
+                end_x = (nx1 - 1) if p + 1 == th else (p + 1) * (nx1 - 2) // th + 1
+
+                blocks = math.ceil((end_x - start_x) / cache_a)
+
+                # Iterate through the cache blocks
+                for b in range(blocks):
+                    start_bx = start_x + b * cache_a
+                    end_bx = end_x if b + 1 == blocks else start_x + (b + 1) * cache_a
+
+                    # Iterate through j, add + 3 for offset for second pass, and vy pass
+                    for i in range(1, ny1 - 1 + step_size):
+                        for j in range(start_bx, end_bx):
+                            # Pass vx
+                            iloc0 = i
+                            if 1 <= j <= nx1 - 2 and 1 <= iloc0 <= ny1 - 1:
+                                vx_new[iloc0, j] = inline_loop_body_vx(i=iloc0, j=j, dx=dx, dy=dy, relax_v=relax_v,
+                                                                       etap=etap, etab=etab,
+                                                                       vx=vx, vy=vy, rhs=vx_rhs)
+
+                            # Pass vy
+                            iloc1 = i - step_size
+                            if 1 <= j <= nx1 - 1 and 1 <= iloc1 <= ny1 - 2:
+                                vy_new[iloc1, j] = inline_loop_body_vy(i=iloc1, j=j, dx=dx, dy=dy, relax_v=relax_v,
+                                                                       etap=etap, etab=etab,
+                                                                       vx=vx_new, vy=vy, rhs=vy_rhs)
+
+            # Copy the results vx_new to vx
+            vx[:] = vx_new[:]
+            vy[:] = vy_new[:]
+
+            apply_vx_BC(vx, BC)
+            apply_vy_BC(vy, BC)
+
+        return vx, vy
+
+    else:
+        velocity_smoother_rb_gs(nx1=nx1, ny1=ny1,
+                                dx=dx, dy=dy,
+                                etap=etap, etab=etab,
+                                vx=vx, vy=vy,
+                                relax_v=relax_v, BC=BC,
+                                vx_rhs=vx_rhs, vy_rhs=vy_rhs, max_iter=max_iter)
+
+        return vx, vy
