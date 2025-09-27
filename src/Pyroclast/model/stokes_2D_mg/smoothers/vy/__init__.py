b312a31a41132e6da9447fd074e38d1de35869b8
AliSot2000 <alisot200@gmail.com>
2025-09-27 18:16:16 +0200

    + Importing renamed functions + Importing gpu inline functinos

--- src/Pyroclast/model/stokes_2D_mg/smoothers/vy/__init__.py
+++ src/Pyroclast/model/stokes_2D_mg/smoothers/vy/__init__.py
@@ -1,1 +1,8 @@
-from _inline_vy import prep_vy_cache, inline_loop_body_vy, compute_neighbor_sum, compute_coeffs
+from _inline_vy import (cpu_prep_vy_cache,
+                        cpu_inline_loop_body_vy,
+                        cpu_compute_neighbor_sum_vy,
+                        cpu_compute_coeffs_vy,
+
+                        gpu_compute_coeffs_vy,
+                        gpu_compute_neighbor_sum_vy,
+                        gpu_inline_loop_body_vy)
