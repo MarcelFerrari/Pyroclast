8a8289ddc7c5a8a349096d3e89dab9fbc829ff31
AliSot2000 <alisot200@gmail.com>
2025-09-29 11:31:42 +0200

    + bugfix, don't need cache

--- src/Pyroclast/model/stokes_2D_mg/smoothers/gat_01.py
+++ src/Pyroclast/model/stokes_2D_mg/smoothers/gat_01.py
@@ -151,7 +151,6 @@ def benchmark_factory() -> tuple[Type["BenchmarkSmoother"], Type["BenchmarkVX"],
     module_name = os.path.basename(__file__).replace(".py", "")
 
     class BaseImplementationBenchmarkSmoother(bw.BenchmarkSmoother):
-        needs_cache_block_size_1: bool = True
         device_arrays: list[DeviceNDArray] = []
 
         def benchmark_preamble(self):
