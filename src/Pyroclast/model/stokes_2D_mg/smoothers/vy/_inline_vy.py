ebdfeb84c145bcf3821ba3cf6ac4035dc7f293da
AliSot2000 <alisot200@gmail.com>
2025-09-29 18:01:25 +0200

    + Added better errors

--- src/Pyroclast/model/stokes_2D_mg/smoothers/vy/_inline_vy.py
+++ src/Pyroclast/model/stokes_2D_mg/smoothers/vy/_inline_vy.py
@@ -60,9 +60,6 @@ def base_compute_neighbor_sum_vy(i: int, j: int, relax_v: float,
 cpu_compute_coeffs_vy = nb.njit(cache=True, inline="always")(base_compute_coeffs_vy)
 cpu_compute_neighbor_sum_vy = nb.njit(cache=True, inline="always")(base_compute_neighbor_sum_vy)
 
-# Setting defaults for gpu
-gpu_inline_loop_body_vy = gpu_compute_neighbor_sum_vy = gpu_compute_coeffs_vy = None
-
 
 @nb.njit(cache=True, inline="always")
 def cpu_inline_loop_body_vy(i: int, j: int,
@@ -132,3 +129,13 @@ if NUMBA_CUDA_AVAIL:
                                            vx=vx, vy=vy, rhs=rhs,
                                            vx_c1=vx_c1, vx_c2=vx_c2, vx_c3=vx_c3, vx_c4=vx_c4,
                                            vy_c1=vy_c1, vy_c2=vy_c2, vy_c3=vy_c3, vy_c4=vy_c4, vy_c5=vy_c5)
+
+else:
+    def gpu_compute_coeffs_vy(*args, **kwargs):
+        raise ImportError("numba-cuda needed for gpu support. Try installing Pyroclast[cuda]")
+
+    def gpu_compute_neighbor_sum_vy(*args, **kwargs):
+        raise ImportError("numba-cuda needed for gpu support. Try installing Pyroclast[cuda]")
+
+    def gpu_inline_loop_body_vy(*args, **kwargs):
+        raise ImportError("numba-cuda needed for gpu support. Try installing Pyroclast[cuda]")
