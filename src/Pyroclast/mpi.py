9fae5d2ae551b81d8d3c8e12cb9f09639e540967
MarcelFerrari <marcel.mfnc@gmail.com>
2025-10-26 12:13:25 +0100

    WIP: removed shift_coords

--- src/Pyroclast/mpi.py
+++ src/Pyroclast/mpi.py
@@ -17,68 +17,12 @@ def create_cart_comm(*args, **kwargs):
 def get_cart_comm():
     return _cart_comm
 
-def halo_exchange_2D(dsts, srcs, wait = True):
+# We use COMM_WORLD rank to shard filenames
+def get_shard_filename(fname):
     if MPI is None:
-        raise RuntimeError("MPI is not available")
-
-    comm = get_cart_comm()
-    coords = comm.Get_coords(comm.Get_rank())
-    dims = comm.Get_topo()[0]
-
-    # Neighbor ranks (None = boundary)
-    def rank(i, j):
-        if 0 <= i < dims[0] and 0 <= j < dims[1]:
-            return comm.Get_cart_rank((i, j))
-        return MPI.PROC_NULL
-
-    i, j = coords
-    neighbors = {
-        'up':    rank(i - 1, j),
-        'down':  rank(i + 1, j),
-        'left':  rank(i, j - 1),
-        'right': rank(i, j + 1),
-        'nw':    rank(i - 1, j - 1),
-        'ne':    rank(i - 1, j + 1),
-        'sw':    rank(i + 1, j - 1),
-        'se':    rank(i + 1, j + 1),
-    }
-
-    # Unpack source and destination buffers (1D or 2D slices)
-    up_src, down_src, left_src, right_src, nw_src, ne_src, sw_src, se_src = srcs
-    up_dst, down_dst, left_dst, right_dst, nw_dst, ne_dst, sw_dst, se_dst = dsts
-
-    reqs = []
-
-    # Cardinal directions
-    if neighbors['up'] != MPI.PROC_NULL:
-        reqs.append(comm.Isend(down_src, dest=neighbors['up']))
-        reqs.append(comm.Irecv(up_dst, source=neighbors['up']))
-    if neighbors['down'] != MPI.PROC_NULL:
-        reqs.append(comm.Isend(up_src, dest=neighbors['down']))
-        reqs.append(comm.Irecv(down_dst, source=neighbors['down']))
-    if neighbors['left'] != MPI.PROC_NULL:
-        reqs.append(comm.Isend(right_src, dest=neighbors['left']))
-        reqs.append(comm.Irecv(left_dst, source=neighbors['left']))
-    if neighbors['right'] != MPI.PROC_NULL:
-        reqs.append(comm.Isend(left_src, dest=neighbors['right']))
-        reqs.append(comm.Irecv(right_dst, source=neighbors['right']))
-
-    # Diagonal directions
-    if neighbors['nw'] != MPI.PROC_NULL:
-        reqs.append(comm.Isend(se_src, dest=neighbors['nw']))
-        reqs.append(comm.Irecv(nw_dst, source=neighbors['nw']))
-    if neighbors['ne'] != MPI.PROC_NULL:
-        reqs.append(comm.Isend(sw_src, dest=neighbors['ne']))
-        reqs.append(comm.Irecv(ne_dst, source=neighbors['ne']))
-    if neighbors['sw'] != MPI.PROC_NULL:
-        reqs.append(comm.Isend(ne_src, dest=neighbors['sw']))
-        reqs.append(comm.Irecv(sw_dst, source=neighbors['sw']))
-    if neighbors['se'] != MPI.PROC_NULL:
-        reqs.append(comm.Isend(nw_src, dest=neighbors['se']))
-        reqs.append(comm.Irecv(se_dst, source=neighbors['se']))
-
-    if wait:
-        MPI.Request.Waitall(reqs)
-        return None
-    else:
-        return reqs
+        return fname
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+    zfill = len(str(size - 1))
+    return f"{fname}.rank_{str(rank).zfill(zfill)}"
