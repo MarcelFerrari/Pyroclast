93fb0200bb1a87b08cb87424a9039f135d2fb5ac
AliSot2000 <alisot200@gmail.com>
2025-11-26 00:01:30 +0100

    + Wrong pinning

--- scripts/thesis/run_benchmarks_pc.sh
+++ scripts/thesis/run_benchmarks_pc.sh
@@ -13,7 +13,7 @@ source $PCD/share/setup-env--cache_alex.sh
 cd $PCD/src/benchmark
 
 # Run small cpu benchmarks
-numactl --physcpubind=0,2,4,6,8,10,12,15 python3 runner.py -m base_jacobi base_rb_gs jacobi_fuse jacobi_fuse_cache jacobi_fuse_cache_jitter jacobi_fuse_cache_unroll_jitter rb_gs_fuse rb_gs_fuse_cache rb_gs_fuse_cache_jitter \
+numactl --physcpubind=0,2,4,6,8,10,12,14 python3 runner.py -m base_jacobi base_rb_gs jacobi_fuse jacobi_fuse_cache jacobi_fuse_cache_jitter jacobi_fuse_cache_unroll_jitter rb_gs_fuse rb_gs_fuse_cache rb_gs_fuse_cache_jitter \
     --cpu 1 2 4 8 \
     --samples 15 \
     --unroll 4 \
@@ -26,7 +26,7 @@ numactl --physcpubind=0,2,4,6,8,10,12,15 python3 runner.py -m base_jacobi base_r
     --dimension 64 90 128 181 256 362 512
 
 # Medium benchmark
-numactl --physcpubind=0,2,4,6,8,10,12,15 python3 runner.py -m base_jacobi base_rb_gs jacobi_fuse jacobi_fuse_cache jacobi_fuse_cache_jitter jacobi_fuse_cache_unroll_jitter rb_gs_fuse rb_gs_fuse_cache rb_gs_fuse_cache_jitter \
+numactl --physcpubind=0,2,4,6,8,10,12,14 python3 runner.py -m base_jacobi base_rb_gs jacobi_fuse jacobi_fuse_cache jacobi_fuse_cache_jitter jacobi_fuse_cache_unroll_jitter rb_gs_fuse rb_gs_fuse_cache rb_gs_fuse_cache_jitter \
     --cpu 4 8 \
     --samples 15 \
     --unroll 4 \
@@ -39,7 +39,7 @@ numactl --physcpubind=0,2,4,6,8,10,12,15 python3 runner.py -m base_jacobi base_r
     --dimension 512 724 1024 1448 2048 2896 4096
 
 # big benchmark
-numactl --physcpubind=0,2,4,6,8,10,12,15 python3 runner.py -m base_jacobi base_rb_gs jacobi_fuse jacobi_fuse_cache jacobi_fuse_cache_jitter jacobi_fuse_cache_unroll_jitter rb_gs_fuse rb_gs_fuse_cache rb_gs_fuse_cache_jitter \
+numactl --physcpubind=0,2,4,6,8,10,12,14 python3 runner.py -m base_jacobi base_rb_gs jacobi_fuse jacobi_fuse_cache jacobi_fuse_cache_jitter jacobi_fuse_cache_unroll_jitter rb_gs_fuse rb_gs_fuse_cache rb_gs_fuse_cache_jitter \
     --cpu 8 \
     --samples 15 \
     --unroll 4 \
@@ -53,7 +53,7 @@ numactl --physcpubind=0,2,4,6,8,10,12,15 python3 runner.py -m base_jacobi base_r
 
 
 # GPU
-numactl --physcpubind=0,2,4,6,8,10,12,15 python3 runner.py -m gpu_jacobi gpu_rb_gs \
+numactl --physcpubind=0,2,4,6,8,10,12,14 python3 runner.py -m gpu_jacobi gpu_rb_gs \
     --cpu 8 \
     --samples 15 \
     --unroll 4 \
@@ -67,7 +67,7 @@ numactl --physcpubind=0,2,4,6,8,10,12,15 python3 runner.py -m gpu_jacobi gpu_rb_
 
 
 # Cache Tests a
-numactl --physcpubind=0,2,4,6,8,10,12,15 python3 runner.py -m jacobi_fuse_cache rb_gs_fuse_cache \
+numactl --physcpubind=0,2,4,6,8,10,12,14 python3 runner.py -m jacobi_fuse_cache rb_gs_fuse_cache \
     --cpu 8 \
     --samples 15 \
     --unroll 4 \
@@ -79,7 +79,7 @@ numactl --physcpubind=0,2,4,6,8,10,12,15 python3 runner.py -m jacobi_fuse_cache 
     --dimension 16384
 
 # run bigger cache test.
-numactl --physcpubind=0,2,4,6,8,10,12,15 python3 runner.py -m jacobi_fuse_cache_unroll_jitter \
+numactl --physcpubind=0,2,4,6,8,10,12,14 python3 runner.py -m jacobi_fuse_cache_unroll_jitter \
     --cpu 8 \
     --samples 15 \
     ---unrollnroll 4 \
