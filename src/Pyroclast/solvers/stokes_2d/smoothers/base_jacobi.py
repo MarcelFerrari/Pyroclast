88064b39943465255da96568b02f7522e8f1f9e7
AliSot2000 <alisot200@gmail.com>
2025-11-26 09:52:48 +0100

    + Fix location

--- src/Pyroclast/solvers/stokes_2d/smoothers/base_jacobi.py
+++ src/Pyroclast/solvers/stokes_2d/smoothers/base_jacobi.py
@@ -40,7 +40,7 @@ def velocity_smoother_jacobi_vx(nx1: int, ny1: int,
                                                    vx=vx, vy=vy, rhs=vx_rhs)
 
 
-@nb.njit(cache=True, parallel=True, inline="always", fastmath=use_fast_math_cpu, parallel=True)
+@nb.njit(cache=True, parallel=True, inline="always", fastmath=use_fast_math_cpu)
 def velocity_smoother_jacobi_vy(nx1: int, ny1: int,
                                 dx: float, dy: float, relax_v: float,
                                 etap: np.ndarray, etab: np.ndarray,
@@ -53,7 +53,7 @@ def velocity_smoother_jacobi_vy(nx1: int, ny1: int,
                                                    vx=vx, vy=vy, rhs=vy_rhs)
 
 
-@nb.njit(cache=True, fastmath=use_fast_math_cpu)
+@nb.njit(cache=True, fastmath=use_fast_math_cpu, parallel=True)
 def velocity_smoother_jacobi(nx1: int, ny1: int,
                              dx: float, dy: float,
                              etap: np.ndarray, etab: np.ndarray,
