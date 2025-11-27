6b5df76ab8f8f44926e72583b3d74ae7f5e02925
AliSot2000 <alisot200@gmail.com>
2025-11-27 15:43:14 +0100

    + Dito

--- scripts/thesis/run_benchmark_cache_piora.sh
+++ scripts/thesis/run_benchmark_cache_piora.sh
@@ -15,11 +15,11 @@ cd $PCD/src/benchmark
 # TODO number of CPUS from previous example
 numactl --physcpubind=0-7 python3 -u runner.py -m jacobi_fuse_cache rb_gs_fuse_cache \
      --cpu 8 \
-     --samples 15 \
+     --samples 3 \
      --unroll 4 \
      --cache_a 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96 100 104 108 112 116 120 124 128 144 160 176 192 208 224 240 256 272 288 304 320 336 352 368 384 400 416 432 448 464 480 496 512 528 544 560 576 592 608 624 640 656 672 688 704 720 736 752 768 784 800 816 832 848 864 880 896 912 928 944 960 976 992 1008 1024 1040 1056 1072 1088 1104 1120 1136 1152 1168 1184 1200 1216 1232 1248 1264 1280 1296 1312 1328 1344 1360 1376 1392 1408 1424 1440 1456 1472 1488 1504 1520 \
      --test smoother \
-     --iterations 4 \
+     --iterations 32 \
      --jitter 3 \
      --dimension 16384
 
@@ -27,11 +27,11 @@ numactl --physcpubind=0-7 python3 -u runner.py -m jacobi_fuse_cache rb_gs_fuse_c
 # TODO number of CPUS from previous example
 numactl --physcpubind=0-7 python3 -u runner.py -m jacobi_fuse_cache_unroll_jitter \
    --cpu 8 \
-   --samples 15 \
+   --samples 3 \
    --unroll 4 \
    --cache_a 16 32 48 64 80 96 112 128 144 160 176 192 208 224 240 256 272 288 304 320 336 352 368 384 400 416 432 448 464 480 496 512 \
    --cache_b 16 32 48 64 80 96 112 128 \
    --test smoother \
-   --iterations 4 \
+   --iterations 32 \
    --jitter 3 \
    --dimension 16384
