8da4ad85aa231f598949eb9bb3bf6ab109cd7fb4
AliSot2000 <alisot200@gmail.com>
2025-11-29 00:36:47 +0100

    + Directory crated for benchmarks

--- scripts/thesis/run_benchmark_restriction_laptop.sh
+++ scripts/thesis/run_benchmark_restriction_laptop.sh
@@ -13,65 +13,79 @@ source $PCD/share/setup-env-alex.sh
 
 cd $PCD/scripts/thesis/
 
+# Create a directory for the benchmark
+dest_dir="$PCD/scripts/thesis/benchmark_results_copy/restriction/$(date -Iminutes)"
+mkdir "$dest_dir"
+
+
 numactl --physcpubind=0,2,4,6,8,10,12,14 python3 -u parallel_restriction.py \
     --scale 2.5 \
     -l 2 \
     -X 16384 -Y 16384 \
     -c 1 2 4 8 \
-    --samples 15
+    --samples 15 \
+    --output $dest_dir
 
 numactl --physcpubind=0,2,4,6,8,10,12,14 python3 -u parallel_restriction.py \
     --scale 2.5 \
     -l 2 \
     -X 8192 -Y 8192 \
     -c 1 2 4 8 \
-    --samples 15
+    --samples 15 \
+    --output $dest_dir
 
 numactl --physcpubind=0,2,4,6,8,10,12,14 python3 -u parallel_restriction.py \
     --scale 2.5 \
     -l 2 \
     -X 4096 -Y 4096 \
     -c 1 2 4 8 \
-    --samples 15
+    --samples 15 \
+    --output $dest_dir
 
 numactl --physcpubind=0,2,4,6,8,10,12,14 python3 -u parallel_restriction.py \
     --scale 2.5 \
     -l 2 \
     -X 2048 -Y 2048 \
     -c 1 2 4 8 \
-    --samples 15
+    --samples 15 \
+    --output $dest_dir
 
 numactl --physcpubind=0,2,4,6,8,10,12,14 python3 -u parallel_restriction.py \
     --scale 2.5 \
     -l 2 \
     -X 1024 -Y 1024 \
     -c 1 2 4 8 \
-    --samples 15
+    --samples 15 \
+    --output $dest_dir
 
 numactl --physcpubind=0,2,4,6,8,10,12,14 python3 -u parallel_restriction.py \
     --scale 2.5 \
     -l 2 \
     -X 512 -Y 512 \
     -c 1 2 4 8 \
-    --samples 15
+    --samples 15 \
+    --output $dest_dir
 
 numactl --physcpubind=0,2,4,6,8,10,12,14 python3 -u parallel_restriction.py \
     --scale 2.5 \
     -l 2 \
     -X 256 -Y 256 \
     -c 1 2 4 8 \
-    --samples 15
+    --samples 15 \
+    --output $dest_dir
 
 numactl --physcpubind=0,2,4,6,8,10,12,14 python3 -u parallel_restriction.py \
     --scale 2.5 \
     -l 2 \
     -X 128 -Y 128 \
     -c 1 2 4 8 \
-    --samples 15
+    --samples 15 \
+    --output $dest_dir
 
 numactl --physcpubind=0,2,4,6,8,10,12,14 python3 -u parallel_restriction.py \
     --scale 2.5 \
     -l 2 \
     -X 64 -Y 64 \
     -c 1 2 4 8 \
-    --samples 15
+    --samples 15 \
+    --output $dest_dir
