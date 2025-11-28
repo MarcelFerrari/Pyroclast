e7ea7b058f58a6dd2d0b3c7fae706b0d1f8c84ed
AliSot2000 <alisot200@gmail.com>
2025-11-28 20:05:02 +0100

    + Added numas to test if it improves the jitter plot

--- scripts/thesis/run_benchmark_scaling_piora.sh
+++ scripts/thesis/run_benchmark_scaling_piora.sh
@@ -13,7 +13,7 @@ cd $PCD/src/benchmark
 
 # Run small cpu benchmarks
 # python3 -u runner.py -D
-python3 -u runner.py -m base_jacobi base_rb_gs jacobi_fuse jacobi_fuse_cache jacobi_fuse_cache_jitter jacobi_fuse_cache_unroll_jitter rb_gs_fuse rb_gs_fuse_cache rb_gs_fuse_cache_jitter \
+numcactl --physcpubind=0-127 python3 -u runner.py -m base_jacobi base_rb_gs jacobi_fuse jacobi_fuse_cache jacobi_fuse_cache_jitter jacobi_fuse_cache_unroll_jitter rb_gs_fuse rb_gs_fuse_cache rb_gs_fuse_cache_jitter \
     --cpu 1 2 4 8 16 32 \
     --samples 15 \
     --unroll 4 \
@@ -25,7 +25,7 @@ python3 -u runner.py -m base_jacobi base_rb_gs jacobi_fuse jacobi_fuse_cache jac
     --dimension 64 90 128
 
 # medium benchmark
-python3 -u runner.py -m base_jacobi base_rb_gs jacobi_fuse jacobi_fuse_cache jacobi_fuse_cache_jitter jacobi_fuse_cache_unroll_jitter rb_gs_fuse rb_gs_fuse_cache rb_gs_fuse_cache_jitter \
+numcactl --physcpubind=0-127 python3 -u runner.py -m base_jacobi base_rb_gs jacobi_fuse jacobi_fuse_cache jacobi_fuse_cache_jitter jacobi_fuse_cache_unroll_jitter rb_gs_fuse rb_gs_fuse_cache rb_gs_fuse_cache_jitter \
     --cpu 1 2 4 8 16 32 64  \
     --samples 15 \
     --unroll 4 \
@@ -37,7 +37,7 @@ python3 -u runner.py -m base_jacobi base_rb_gs jacobi_fuse jacobi_fuse_cache jac
     --dimension 181 256 362 512
 
 # big benchmark
-python3 -u runner.py -m jacobi_fuse_cache jacobi_fuse_cache_unroll_jitter rb_gs_fuse_cache \
+numcactl --physcpubind=0-127 python3 -u runner.py -m jacobi_fuse_cache jacobi_fuse_cache_unroll_jitter rb_gs_fuse_cache \
     --cpu 4 8 16 32 64 96 128 \
     --samples 15 \
     --unroll 4 \
@@ -49,7 +49,7 @@ python3 -u runner.py -m jacobi_fuse_cache jacobi_fuse_cache_unroll_jitter rb_gs_
     --dimension 724 1024 1448 2048 2896 4096
 
 # GPU small
-python3 -u runner.py -m gpu_jacobi gpu_rb_gs \
+numcactl --physcpubind=0-127 python3 -u runner.py -m gpu_jacobi gpu_rb_gs \
     --cpu 8 \
     --samples 15 \
     --unroll 4 \
@@ -61,7 +61,7 @@ python3 -u runner.py -m gpu_jacobi gpu_rb_gs \
     --dimension 64 90 128
 
 # gpu medium
-python3 -u runner.py -m gpu_jacobi gpu_rb_gs \
+numcactl --physcpubind=0-127 python3 -u runner.py -m gpu_jacobi gpu_rb_gs \
     --cpu 8 \
     --samples 15 \
     --unroll 4 \
@@ -73,7 +73,7 @@ python3 -u runner.py -m gpu_jacobi gpu_rb_gs \
     --dimension 181 256 362 512
 
 # GPU big
-python3 -u runner.py -m gpu_jacobi gpu_rb_gs \
+numcactl --physcpubind=0-127 python3 -u runner.py -m gpu_jacobi gpu_rb_gs \
     --cpu 8 \
     --samples 15 \
     --unroll 4 \
